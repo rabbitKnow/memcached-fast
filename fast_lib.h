1017f8a9383dea28600d07263ad6aee4a6f84650
rabbitKnow <honjunzh@gmail.com>
2017-03-13 20:44:43 +0800

    first commit

---
+++ fast_lib.h
@@ -0,0 +1,21 @@
+#include <sys/socket.h>
+
+
+struct fast_socket{
+	uint32_t ip;
+	struct rte_ring *send_ring;
+	struct rte_ring *recv_ring;
+	struct rte_mempool *mem_pool;
+};
+
+int fast_lib_init(void);
+
+struct fast_socket * create_fast_socket(void);
+
+int fast_bind(struct fast_socket *socket,const char *addr);
+
+int fast_sendto(struct fast_socket * socket,const void *buf,size_t len,int flags,uint32_t dest_addr);
+
+int fast_sendmsg(struct fast_socket * socket,const struct msghdr *msg,int flags);
+
+int fast_recvfrom(struct fast_socket * socket, void *buf, size_t len, int flags,uint32_t src_addr);
