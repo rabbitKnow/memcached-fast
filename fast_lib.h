84e6f0a0951ce56a05e18a63681dfea283dfe532
rabbitK <honjunzh@gmail.com>
2017-06-18 17:35:02 +0800

    debug

--- fast_lib.h
+++ fast_lib.h
@@ -27,6 +27,9 @@ struct fast_socket{
 	struct rte_mempool *mem_pool[8];
 	struct fast_mbuf_array *array[8];
 	struct fast_mbuf_array *recv_buf[8];
+	uint8_t *src_mac_be[8];
+	uint8_t *dest_mac_be[8];
+	uint32_t src_ip[8];
 };
 
 int fast_lib_init(void);
@@ -35,8 +38,8 @@ struct fast_socket * create_fast_socket(void);
 
 int fast_bind(struct fast_socket *socket,const char *addr);
 
-int fast_sendto(struct fast_socket * socket,const void *buf,size_t len,int flags,const struct sockaddr *dest_addr, socklen_t addrlen, uint16_t queue);
+int fast_sendto(struct fast_socket * socket,const void *buf,size_t len,int flags,const struct sockaddr *dest_addr, socklen_t addrlen, uint16_t port);
 
-int fast_sendmsg(struct fast_socket * socket,const struct msghdr *msg,int flags, uint16_t queue);
+int fast_sendmsg(struct fast_socket * socket,const struct msghdr *msg,int flags, uint16_t port);
 
-int fast_recvfrom(struct fast_socket * socket, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen,uint16_t queue);
+int fast_recvfrom(struct fast_socket * socket, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen,uint16_t port);
